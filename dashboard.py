587910e6122bbd266535f10ae0fd45836f9c84d3
PJC2010 <castillop92@gmail.com>
2025-03-19 16:14:52 -0500

    one time

--- dashboard.py
+++ dashboard.py
@@ -71,7 +71,7 @@ def get_available_weeks(num_weeks=12):
         EXTRACT(YEAR FROM DataAsOfDate) AS Year,
         MAX(DataAsOfDate) AS LastDataAsOfDate
     FROM `medadhdata2025.adherence_tracking.weekly_med_adherence_data`
-    GROUP BY WeekNumber, Year
+    GROUP BY DataAsOfDate WeekNumber, Year
     ORDER BY DataAsOfDate DESC
     LIMIT {num_weeks}
     """
